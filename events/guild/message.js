2a27209a41cad5bafe7168189c02455e940be370
Tyler Catus <TY3Catus@gmail.com>
2023-10-03 15:32:43 -0400

    Fixed formatting

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,14 +1,8 @@
 module.exports = (client, Discord, message) => {
- 
-
     try{
         // command.execute(client, message, Discord);
     } catch(err){
         message.reply("There was an error trying to execute this command!");
         console.log(err);
     }
-
-
-
-
 }
